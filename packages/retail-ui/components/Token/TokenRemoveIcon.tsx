ead4d9ce013d6b811ad9e2ebd6321c60351ba1c2
wKich <nflazzer@gmail.com>
2018-11-21 15:22:20 +0500

    style(Token): reformat

--- packages/retail-ui/components/Token/TokenRemoveIcon.tsx
+++ packages/retail-ui/components/Token/TokenRemoveIcon.tsx
@@ -14,7 +14,8 @@ const TokenRemoveIcon = (props: RemoveIconProps) => (
     strokeLinejoin="round"
     strokeMiterlimit="1.414"
     clipRule="evenodd"
-    viewBox="0 0 14 14">
+    viewBox="0 0 14 14"
+  >
     <rect width={14} height={14} y=".288" fill="none" />
     <path d="M7.62,7.295l5.404,5.404l-0.608,0.608l-5.404,-5.404l-5.404,5.404l-0.608,-0.608l5.404,-5.404l-5.404,-5.404l0.608,-0.608l5.404,5.404l5.404,-5.404l0.608,0.608l-5.404,5.404Z" />
   </svg>
